99954d5cfd1bf5fa20c867312cda02b4524541c9
tlarnold10 <tlarnold10@gmail.com>
2022-07-16 10:54:38 -0500

    added projects and arnold tech pages

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +8,9 @@ import { NavComponent } from './navigation/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
-import { RouterModule } from '@angular/router';
 import { ExperienceComponent } from './experience/experience.component'
+import { ProjectsComponent } from './projects/projects.component'
+import { ArnoldTechComponent } from './arnoldtech/arnoldtech.component'
 
 @NgModule({
   declarations: [
@@ -17,17 +19,21 @@ import { ExperienceComponent } from './experience/experience.component'
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    ProjectsComponent,
+    ArnoldTechComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'about', component: AboutComponent},
-      {path: 'experience', component: ExperienceComponent}
+      {path: 'experience', component: ExperienceComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'arnoldtech', component: ArnoldTechComponent}
     ])
   ],
   providers: [],
-  bootstrap: [AppComponent, NavComponent, FooterComponent, HomeComponent, AboutComponent, ExperienceComponent]
+  bootstrap: [AppComponent, NavComponent, FooterComponent, HomeComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ArnoldTechComponent]
 })
 export class AppModule { }
