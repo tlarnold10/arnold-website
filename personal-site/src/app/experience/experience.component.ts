d91d72a858929b16ac8ebc9f926c92295c31024a
tarnold <tarnold@fti-net.com>
2024-08-12 16:05:46 -0500

    a few updates

--- personal-site/src/app/experience/experience.component.ts
+++ personal-site/src/app/experience/experience.component.ts
@@ -9,17 +9,20 @@ export class ExperienceComponent {
 
     constructor() {
         this.experiences = [
-            "Graduate from Augustana College in Sioux Falls. Computer Information System and Business Administration. 2014 graduate.",
-            "Sales Account Executive at Omnitech.",
-            "Start MBA program at University of South Dakota.",
-            "Data conversion engineer at Zuercher Technologies in Sioux Falls.",
-            "Zuercher Technologies becomes CentralSquare Technologies.",
-            "Data conversion manager at CentralSquare Technologies.",
-            "Graduate from USD with Masters of Business Administration, December 2019.",
-            "Move to Lester, IA",
-            "Starting up Arnold Technologies.",
-            "Software engineering manager at CentralSquare Technologies.",
-            "Software engineer at Frontier Technology Inc."
+            "May 2014: Graduate from Augustana College in Sioux Falls. Computer Information System and Business Administration",
+            "January 2015: Sales Account Executive at Omnitech",
+            "May 2018: Data conversion engineer at Zuercher Technologies in Sioux Falls",
+            "2019: Zuercher Technologies becomes CentralSquare Technologies",
+            "May 2019: Data conversion manager at CentralSquare Technologies",
+            "December 2019: Graduate from USD with Masters of Business Administration",
+            "2020: Move to Lester, IA",
+            "July 2020: Starting up Arnold Technologies, building estimation tool for local appliance store",
+            "June 2021: Software engineering manager at CentralSquare Technologies",
+            "August 2022: Associate software engineer at Frontier Technology Inc", 
+            "2023: Arnold Technologies software development consulting with JavaScript, htmx, and Ruby",
+            "August 2023: Promotion to software engineer",
+            "2024: Arnold Technologies software development consulting with JavaScript + Vue.js",
+            "July 2024: Promotion to senior software engineer"
         ];
     }
 }
